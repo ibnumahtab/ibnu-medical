caa23f097aaf1eac3ac9dc0c59a8bdbc7b2da071
ibnumahtab <ibnumahtab@gmail.com>
2021-10-18 18:37:29 +0600

    Navbar Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Navbar from './components/Header/Navbar/Navbar';
 import firebaseInit from './Firebase/Firebase.init';
 
 firebaseInit();
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Navbar />
+        </div>
+    );
 }
 
 export default App;
