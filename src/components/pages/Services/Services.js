00bb180cd4c7be39f5c0d6ad044f0dad3e8f5a27
ibnumahtab <ibnumahtab@gmail.com>
2021-10-19 16:13:08 +0600

    Service with Responsive Complete

--- src/components/pages/Services/Services.js
+++ src/components/pages/Services/Services.js
@@ -3,30 +3,32 @@ import useService from '../../../hooks/useService';
 import SingleService from '../../SingleService/SingleService';
 
 const Services = () => {
-        const [services] = useService();
-        return (
-            <div className='bg-secondary'>
-                <div className='container mx-auto py-12'>
-                    <p className='text-lg text-center'>
+    const [services] = useService();
+    return (
+        <div>
+            <div className="container mx-auto py-12">
+                <div className="pb-5 px-5 md:px-0">
+                    <p className="text-lg text-center hidden md:block">
                         The Best Medical And General Practice Care!
                     </p>
-                    <h2 className='text-4xl primary-color text-center font-bold my-4'>
+                    <h2 className="text-4xl primary-color text-center font-bold my-4">
                         Total Health Care Solutions
                     </h2>
-                    <h2 className='text-2xl text-center pb-8'>
+                    <h2 className="text-2xl text-center pb-8">
                         Providing Medical Care For The Sickest In Our Community.
                     </h2>
-                    <div className='grid grid-cols-3 gap-4'>
-                        {services.map((service) => (
-                            <SingleService
-                                key={service.id}
-                                service={service}
-                            ></SingleService>
-                        ))}
-                    </div>
+                </div>
+                <div className="grid grid-cols-1 m-5 md:m-0 md:grid-cols-4 gap-4">
+                    {services.map(service => (
+                        <SingleService
+                            key={service.id}
+                            service={service}
+                        ></SingleService>
+                    ))}
                 </div>
             </div>
-        );
+        </div>
+    );
 };
 
 export default Services;
--- src/components/pages/SingleTestDetail/SingleTestDetail.js
+++ src/components/pages/SingleTestDetail/SingleTestDetail.js
@@ -3,21 +3,25 @@ import { useParams } from 'react-router';
 import useTest from '../../../hooks/useTest';
 
 const SingleTestDetail = () => {
-    const { id } = useParams()
-    const [tests] = useTest([])
-    const test = tests?.find(val => val.testId === id)
+    const { id } = useParams();
+    const [tests] = useTest([]);
+    const test = tests?.find(val => val.testId === id);
     console.log(test);
     return (
         <div>
-            <div className='container mx-auto py-12'>
-                <div className='flex'>
-                    <div className='w-1/3'>
-                        <img src={test?.testImg} alt='' />
-                        <h2 className="text-2xl mt-4 py-2 font-bold">Current Fee: $ {test?.testCurrFee}</h2>
-                        <h2 className="text-2xl py-2 font-bold">Previous Fee: $ {test?.testPrevFee}</h2>
+            <div className="container mx-auto py-12">
+                <div className="flex">
+                    <div className="w-1/3">
+                        <img src={test?.testImg} alt="" />
+                        <h2 className="text-2xl mt-4 py-2 font-bold">
+                            Current Fee: $ {test?.testCurrFee}
+                        </h2>
+                        <h2 className="text-2xl py-2 font-bold">
+                            Previous Fee: $ {test?.testPrevFee}
+                        </h2>
                     </div>
-                    <div className='w-2/3 pl-8'>
-                        <h2 className='text-4xl primary-color mb-4'>
+                    <div className="w-2/3 pl-8">
+                        <h2 className="text-4xl primary-color mb-4">
                             {test?.testName}
                         </h2>
                         <p>{test?.testFullDesc}</p>
