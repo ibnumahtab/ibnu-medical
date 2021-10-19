c7bd70dad1fe1fef3b15abfc71be79230db045c3
ibnumahtab <ibnumahtab@gmail.com>
2021-10-19 17:37:20 +0600

    Single Doctor Responsive Complete

--- src/components/pages/Doctors/Doctors.js
+++ src/components/pages/Doctors/Doctors.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import useDoctor from "../../../hooks/useDoctor";
+import useDoctor from '../../../hooks/useDoctor';
 import SingleDoctor from '../../SingleDoctor/SingleDoctor';
 const Doctors = () => {
     const [doctors] = useDoctor();
 
     return (
-        <div className='container mx-auto py-8'>
-            <h2 className='text-5xl py-8 text-center primary-color font-bold'>
-                Meet Our Doctors
-            </h2>
-            <p className='text-center text-xl w-2/3 mx-auto mb-12'>
-                Our administration and support staff all have exceptional people
-                skills and trained to assist you with all medical enquiries.
-            </p>
-            <div className='grid grid-cols-4 gap-8'>
-                {doctors.map((doctor) => (
+        <div className="container mx-auto py-8">
+            <div>
+                <h2 className="text-3xl md:text-5xl pb-4 md:py-8 text-center primary-color font-bold">
+                    Meet Our Doctors
+                </h2>
+                <p className="md:text-center px-5 md:px-0 text-xl text-justify md:w-2/3 mx-auto mb-12">
+                    Our administration and support staff all have exceptional
+                    people skills and trained to assist you with all medical
+                    enquiries.
+                </p>
+            </div>
+            <div className="grid grid-cols-1 m-5 md:m-0 md:grid-cols-4 gap-8">
+                {doctors.map(doctor => (
                     <SingleDoctor
                         key={doctor.docId}
                         doctor={doctor}
--- src/components/pages/SingleDoctorDetail/SingleDoctorDetail.js
+++ src/components/pages/SingleDoctorDetail/SingleDoctorDetail.js
@@ -2,59 +2,54 @@ import { faEnvelope, faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { useParams } from 'react-router';
-import useDoctor from '../../../hooks/useDoctor'
-import doctorProfile from '../../../img/doctor-profile.jpg'
+import useDoctor from '../../../hooks/useDoctor';
 
 const SingleDoctorDetail = () => {
-    const {id} = useParams()
-    const [doctors] = useDoctor([])
-    const doctor = doctors?.find(val => val.docId === id)
-    const emailIcon = <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
+    const { id } = useParams();
+    const [doctors] = useDoctor([]);
+    const doctor = doctors?.find(val => val.docId === id);
+    const emailIcon = <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>;
     const phoneIcon = <FontAwesomeIcon icon={faPhoneAlt}></FontAwesomeIcon>;
     return (
         <div>
-            <div
-                className='py-24'
-                style={{
-                    backgroundImage: `url(${doctorProfile})`,
-                    backgroundAttachment: "fixed",
-                    backgroundSize: "cover",
-                }}
-            >
-                <div className='container mx-auto flex'>
-                    <div className='w-1/2'></div>
-                    <div className='w-1/2'>
-                        <h2 className='text-white  text-4xl'>
-                            {doctor?.docName}
-                        </h2>
-                        <h2 className='text-white  text-2xl'>
-                            {doctor?.docPosition}
-                        </h2>
-                    </div>
-                </div>
-            </div>
-            <div className='container mx-auto'>
-                <div className='flex'>
-                    <div className='w-2/5 pr-12 '>
-                        <img className='-mt-32' src={doctor?.docImg} alt='' />
+            <div className="container mx-auto py-28 px-10 md:px-0">
+                <div className="md:flex md:justify-center md:items-center">
+                    <div className="md:w-2/5 pt-24">
+                        <img
+                            className="-mt-32 rounded-3xl"
+                            src={doctor?.docImg}
+                            alt=""
+                        />
+                        <div className="py-5">
+                            <h2 className="text-4xl">{doctor?.docName}</h2>
+                            <h2 className="text-2xl">{doctor?.docPosition}</h2>
+                        </div>
                         <div>
-                            <h2 className="text-center text-3xl mt-8 primary-color font-bold">Contact Info</h2>
-                            <div>
-                                <h2>{phoneIcon} {doctor?.docContact?.mob}</h2>
-                                <h2>{emailIcon} {doctor?.docContact?.email}</h2>
+                            <h2 className="text-center text-3xl mt-8 primary-color font-bold">
+                                Contact Info
+                            </h2>
+                            <div className="text-center py-5 text-xl">
+                                <h2>
+                                    {phoneIcon} {doctor?.docContact?.mob}
+                                </h2>
+                                <h2>
+                                    {emailIcon} {doctor?.docContact?.email}
+                                </h2>
                             </div>
                         </div>
                     </div>
-                    <div className='w-3/5 pt-4 pl-12'>
-                        <h2 className='text-4xl pb-4'>About Me:</h2>
-                        <p>{doctor?.docDesc}</p>
-                        <div className='grid grid-cols-1 gap-y-4'>
-                            {doctor?.experience.map((exp) => (
-                                <div className='bg-secondary p-4 rounded'>
-                                    <h2 className='text-white text-2xl font-bold'>
+                    <div className="md:w-3/6 md:pl-12">
+                        <h2 className="text-4xl py-4">Details:</h2>
+                        <p className="leading-8 text-xl pb-6">
+                            {doctor?.docDesc}
+                        </p>
+                        <div className="grid grid-cols-1 gap-y-4">
+                            {doctor?.experience.map(exp => (
+                                <div className="border p-4 rounded">
+                                    <h2 className="text-2xl font-bold">
                                         {exp?.hospitalName}
                                     </h2>
-                                    <h2 className='my-4 primary-color text-xl font-bold'>
+                                    <h2 className="pb-3 primary-color text-xl font-bold">
                                         {exp?.year}
                                     </h2>
                                     <p>{exp?.description}</p>
