a55eb3e154ae80f59272ad671f5a5aaf4d78b114
ibnumahtab <ibnumahtab@gmail.com>
2021-10-19 11:36:36 +0600

    Header Complete

--- src/components/AppointmentSection/AppointmentSection.js
+++ src/components/AppointmentSection/AppointmentSection.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import sectionBg from '../../img/section-bg.jpg'
+import sectionBg from '../../img/section-bg.jpg';
 
 const AppointmentSection = () => {
     return (
         <div
-            className=''
+            className=""
             style={{
                 background: `url(${sectionBg})`,
-                backgroundAttachment: "fixed",
-                backgroundSize: "cover",
+                backgroundAttachment: 'fixed',
+                backgroundSize: 'cover',
             }}
         >
-            <div className='container mx-auto py-12'>
-                <h2 className='text-xl text-center text-white'>
+            <div className="container mx-auto py-12">
+                <h2 className="text-xl text-center text-white">
                     Need a Doctor for Check-up?
                 </h2>
-                <h2 className='text-4xl py-4 text-white text-center'>
+                <h2 className="text-4xl py-4 text-white text-center">
                     Just Make An Appointment & You're Done!
                 </h2>
                 <div className="text-center">
-                    <button className='px-4 py-2 border border-white text-white'>
+                    <button className="px-4 py-2 border border-white text-white">
                         Make An Appointment
                     </button>
                 </div>
--- src/components/Header/TopHeader.js/TopHeader.js
+++ src/components/Header/TopHeader.js/TopHeader.js
@@ -1,51 +1,63 @@
-
-import { faAddressCard, faClock } from '@fortawesome/free-regular-svg-icons';
-import { faSignInAlt, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
+import {
+    faMapMarkerAlt,
+    faSignInAlt,
+    faUser,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
-import logo from '../../../img/logo.png'
 const TopHeader = () => {
-    const clockIcon = <FontAwesomeIcon icon={faClock}></FontAwesomeIcon>
-    const addressIcon = <FontAwesomeIcon icon={faAddressCard}></FontAwesomeIcon>
-    const loginIcon = <FontAwesomeIcon icon={faSignInAlt}></FontAwesomeIcon>
-    const userIcon = <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
-    const {user, logOut} = useAuth()
+    const clockIcon = <FontAwesomeIcon icon={faClock}></FontAwesomeIcon>;
+    const mapMarkerIcon = (
+        <FontAwesomeIcon icon={faMapMarkerAlt}></FontAwesomeIcon>
+    );
+    const loginIcon = <FontAwesomeIcon icon={faSignInAlt}></FontAwesomeIcon>;
+    const userIcon = <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>;
+    const { user, logOut } = useAuth();
     return (
-        <div className='flex py-2'>
-            <div className='w-1/3'>
-                <div className='logo'>
-                    <img className='w-48' src={logo} alt='' />
+        <div className="flex py-2">
+            <div className="w-1/3">
+                <div className="logo">
+                    <img
+                        className="w-48"
+                        src="https://i.ibb.co/ZVJXcnq/Ibnu-Medical-Center-Logo.png"
+                        alt=""
+                    />
                 </div>
             </div>
-            <div className='w-2/3 flex justify-between items-center'>
-                <div className='flex items-center'>
-                    <div className='text-4xl text-blue-500'>{clockIcon}</div>
-                    <div className='text-sm pl-2'>
+            <div className="w-2/3 flex justify-evenly items-center">
+                <div className="flex items-center border p-3 shadow-sm rounded">
+                    <div className="text-4xl text-indigo-800">{clockIcon}</div>
+                    <div className="text-base pl-2">
                         <p>Mon – Fri : - 8:00 AM - 8:00 PM</p>
                         <p>Sat - Sun : - Closed</p>
                     </div>
                 </div>
-                <div className='flex items-center'>
-                    <div className='text-4xl text-blue-500'>{addressIcon}</div>
-                    <div className='text-sm pl-2'>
+                <div className="flex items-center border p-3 shadow-sm rounded">
+                    <div className="text-4xl text-indigo-800">
+                        {mapMarkerIcon}
+                    </div>
+                    <div className="text-base pl-2">
                         <p>121 King Street, Melbourne,</p>
                         <p>Victoria 3000 Australia</p>
                     </div>
                 </div>
                 <div>
-                    {
-                        user?.email ? (<div>
-                            <span className="border-2 border-black px-2 py-1 rounded-full">{userIcon}</span>
-                        </div>) : (
-                                (<div className='text-right'>
-                    <button className='bg-color text-white rounded px-4 py-1'>
-                        <Link to="/login">{loginIcon} Login</Link>
-                    </button>
-                        </div>)
-                )
-                    }
+                    {user?.email ? (
+                        <div>
+                            <span className="border-2 border-black px-2 py-1 rounded-full">
+                                {userIcon}
+                            </span>
+                        </div>
+                    ) : (
+                        <div className="text-right">
+                            <button className="bg-color text-white text-xl rounded p-3 px-5">
+                                <Link to="/login">{loginIcon} Login</Link>
+                            </button>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
