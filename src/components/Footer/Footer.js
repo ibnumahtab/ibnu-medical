428e5952a073f0224cce249989734d29af7be9a1
ibnumahtab <ibnumahtab@gmail.com>
2021-10-19 23:42:51 +0600

    Footer Added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import FooterTop from './FooterTop/FooterTop';
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className="bg-white">
+            <FooterTop></FooterTop>
+            <p className="text-center pb-5 text-xl">
+                Copyright &copy; 2021 | All Right Reserved!
+            </p>
         </div>
     );
 };
---
+++ src/components/Footer/FooterTop/FooterTop.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const FooterTop = () => {
+    return (
+        <div>
+            <div className="container py-12 mx-auto">
+                <div className="grid md:grid-cols-3 gap-8 grid-cols-1">
+                    <div className="">
+                        <img
+                            src="https://i.ibb.co/ZVJXcnq/Ibnu-Medical-Center-Logo.png"
+                            alt=""
+                        />
+                        <p className="py-4">
+                            Our goal is to deliver quality of care in a
+                            courteous, respectful, and compassionate manner. We
+                            hope you will allow us to care for you and strive to
+                            be the first and best choice for your family
+                            healthcare.
+                        </p>
+                    </div>
+
+                    <div className="border rounded-lg p-8">
+                        <div className="text-center">
+                            <h2 className="text-2xl">Quick Contacts</h2>
+                            <p className="pb-5">
+                                If you have any questions or need help, feel
+                                free to contact with our team.
+                            </p>
+                            <h2 className="text-4xl">01061245741</h2>
+                            <p>
+                                2307 Beverley Rd Brooklyn, New York 11226 United
+                                States.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="border rounded-lg p-8">
+                        <div className="text-center">
+                            <h2 className="text-2xl">Quick Contacts</h2>
+                            <p className="pb-5">
+                                If you have any questions or need help, feel
+                                free to contact with our team.
+                            </p>
+                            <h2 className="text-4xl">01061245741</h2>
+                            <p>
+                                2307 Beverley Rd Brooklyn, New York 11226 United
+                                States.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FooterTop;
