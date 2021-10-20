f736198e86e09f3d1fe6fffe5dfbfdbabbc87d63
ibnumahtab <ibnumahtab@gmail.com>
2021-10-20 12:13:18 +0600

    User Profile Added

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -4,29 +4,39 @@ import React from 'react';
 import useAuth from '../../hooks/useAuth';
 
 const UserProfile = () => {
-    const {user, logOut} = useAuth()
-    const userIcon = <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
+    const { user, logOut } = useAuth();
+    const userIcon = <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>;
+    console.log(user);
     return (
-        <div className="w-48 bg-color absolute right-0 rounded text-white text-center z-50 overflow-auto ">
+        <div className="w-48 border absolute right-0 rounded py-5 bg-white text-center z-50 overflow-auto ">
             <div className="container mx-auto">
-                <div>
-                    {
-                        user?.photeUrl ? <img src={user.photoUrl} alt="" /> : <div className="text-4xl py-4">{userIcon}</div>
-                    }
+                <div className="py-3">
+                    {user?.photoURL ? (
+                        <img
+                            className="rounded-full mx-auto"
+                            src={user.photoURL}
+                            alt=""
+                        />
+                    ) : (
+                        <div className="text-4xl py-4">{userIcon}</div>
+                    )}
                 </div>
                 <div>
-                    <h2>{user.displayName}</h2>
+                    <h2 className="text-lg">{user.displayName}</h2>
+                    <h2 className="text-sm">{user.email}</h2>
                     <div>
-                        <button className="bg-white my-4 px-2 py-1 rounded primary-color">View Profile</button>
+                        <button className="bg-white my-4 px-2 py-1 rounded primary-color">
+                            View Profile
+                        </button>
                     </div>
                     <hr />
-                    <div className="text-left px-4 pb-4">
-                        <h2>Appointment History</h2>
-                        <h2>Last Visited Doctor</h2>
-                        <h2>Favourite Doctors</h2>
-                    </div>
                     <div>
-                        <button className="bg-white px-4 py-1 primary-color mb-4" onClick={logOut}>LogOut</button>
+                        <button
+                            className="bg-white px-4 pt-4 primary-color"
+                            onClick={logOut}
+                        >
+                            LogOut
+                        </button>
                     </div>
                 </div>
             </div>
